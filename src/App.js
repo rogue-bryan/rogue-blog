488309794b625f413f9ec46cc7189ddd1d626e81
Bryan Rengo <bryan.rengo@gmail.com>
2018-07-05 15:25:34 -0500

    fix font loading weirdness

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import { Router, Link, Head } from 'react-static'
-import { hot } from 'react-hot-loader'
-//
 import Routes from 'react-static-routes'
+import { hot } from 'react-hot-loader'
 
 import 'normalize.css'
 import './app.scss'
@@ -12,7 +11,6 @@ const App = () => (
     <div>
       <Head>
         <title>Bryan's Tech Blogger</title>
-        <link href="https://fonts.googleapis.com/css?family=Gaegu" rel="stylesheet"></link>
       </Head>
       <div className="paper">
         <nav>
@@ -20,7 +18,7 @@ const App = () => (
           <Link to="/about">About</Link>
           <Link to="/blog">Blog</Link>
         </nav>
-        <section className="body">
+        <section>
           <Routes />
         </section>
       </div>
