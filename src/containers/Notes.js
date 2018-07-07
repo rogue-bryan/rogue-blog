8b325b46bac3caf9cbdc1e58ac69a5329b3f9d44
Bryan Rengo <bryan.rengo@gmail.com>
2018-07-07 00:10:15 -0500

    add recent posts component

---
+++ src/containers/Notes.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { withRouteData, Link } from 'react-static'
+
+export default withRouteData(({ posts }) => {
+
+    if (posts) {
+        return (
+            <div className="note">
+                <h2>Recent Posts</h2>
+                <ul>
+                {posts.map(post => (
+                    <li key={post.slug}>
+                        <Link to={`/blog/post/${post.slug}/`}>{post.title}</Link>
+                    </li>
+                ))}
+                </ul>
+            </div>
+        )
+    } else {
+        return null
+    }
+})
--- src/containers/Page.js
+++ src/containers/Page.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import { withRouteData } from 'react-static'
 import convert from 'htmr'
-//
+import Notes from './Notes'
 
-export default withRouteData(({ data }) => (
+export default withRouteData(({ data }) => {
+  const posts = data.posts;
+
+  return (
   <div>
+    <Notes posts={posts}/>
     <h2>{data.title}</h2>
     <div>
       {convert(data.contents)}
     </div>
   </div>
-))
+  )
+})
