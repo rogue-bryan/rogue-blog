1c8d8f0dacc0a99178e5d072180a4ddc35939bd7
Bryan Rengo <bryan.rengo@gmail.com>
2018-12-09 09:11:17 -0600

    switched to directory-tree to render object for generating pages

--- src/render.markdown.js
+++ src/render.markdown.js
@@ -1,80 +1,34 @@
-import matter from 'gray-matter'
-import fs from 'fs-extra'
-import pathResolve from 'path'
-import remark from 'remark'
-import html from 'remark-html'
-import walk from 'walk'
+import fs from 'fs';
+import html from 'remark-html';
+import matter from 'gray-matter';
+import remark from 'remark';
+import dirTree from 'directory-tree';
 
-const remarkParser = remark().use(html)
+const remarkParser = remark().use(html);
 
-export default (rootPath) => {
-  return new Promise((resolve, reject) => {
-
-    const root = rootPath || __dirname; // if there's no rootPath use exec location
-    const tree = {};
-    const nodesMap = {};
-    const walker = walk.walk(root, { followLinks: false }); // filter doesn't work well
-
-    const addNode = (node, path) => {
-      if (node.name.indexOf('.') === 0 || path.indexOf('/.') >= 0) { // ignore hidden files
-        return;
-      }
-
-      var relativePath = path.replace(root, '');
-
-      node.path = relativePath + '/' + node.name;
-      nodesMap[node.path] = node;
-
-      if (relativePath.length === 0) { //is root
-        tree[getNodeName(node.name)] = node;
-        return;
-      }
-
-      const parentPath = node.path.substring(0, node.path.lastIndexOf('/'));
-      const parent = nodesMap[parentPath];
-      
-      parent.children.push(node);
-
-    }
-
-    const getMarkdown = async (path, fileName) => {
-      const ext = pathResolve.extname(fileName)
-      if (ext == '.md') {
-        const { data, content } = matter(await fs.readFile(pathResolve.resolve(path, fileName), 'utf8'))
-        const { contents } = await remarkParser.process(content)
-        return {
-          contents: contents,
-          ...data,
-        }
-      } else {
-        return {}
-      }
-    }
-
-    const getNodeName = (name) => {
-      return pathResolve.parse(name).name
-    }
-
-    walker.on('directory', (path, stats, next) => {
-      addNode({ name: stats.name, children: [] }, path);
-      next();
-    });
-
-    walker.on('file', async (path, stats, next) => {
-      addNode({ 
-        name: stats.name,
-        ...await getMarkdown(path, stats.name)
-       }, path);
-      next();
-    });
-
-    walker.on('end', () => {  
-      resolve(tree);
-    });
-
-    walker.on('errors', (root, nodeStatsArray, next) => {
-      reject(nodeStatsArray);
-      next();
-    });
-  });
+const getMarkdown = (filePath) => {
+  console.log(filePath);
+  const { data, content } = matter(fs.readFileSync(filePath, 'utf8'));
+  const { contents } = remarkParser.processSync(content);
+  
+  return {
+    contents: contents.replace(/(\r\n\t|\n|\r\t)/gm,""),
+    ...data,
+  };
 }
+
+export default (rootPath, fileRegex = /\.md/) => {
+  const rootNode = rootPath || __dirname;
+  const filter = {extensions: fileRegex};
+
+  const tree = dirTree(
+    rootNode,
+    filter, 
+    (item, path) => {
+      item = Object.assign(item, {}, {
+        ...getMarkdown(item.path)
+      });
+    });
+
+  return tree;
+};
--- src/render.markdown.spec.js
+++ src/render.markdown.spec.js
@@ -5,23 +5,6 @@ describe('render.markdown', () => {
     it('when given a folder of md files, expect the resulting json to match', async () => {
         const dir = path.resolve(__dirname, './__tests__/content')
         const json = await renderMarkdown(dir)
-        expect(json).toEqual({
-            'first-post': {
-                'contents': `<p>First post! Blogging is now enabled using Markdown!</p>
-`, 'name': 'first-post.md', 'path': '/first-post.md', 'slug': 'first-post', 'title': 'First Post!'
-            }, 'home': {
-                'children': [{
-                    'contents': `<h1>Bryan's Tech Blog</h1>
-<p>Lorem</p>
-`, 'name': 'home.md', 'path': '/home/home.md'
-                }, {
-                    'children': [{
-                        'contents': `<h1>Bryan's Tech Blog</h1>
-<p>Lorem</p>
-`, 'name': 'second.md', 'path': '/home/second/second.md'
-                    }], 'name': 'second', 'path': '/home/second'
-                }], 'name': 'home', 'path': '/home'
-            }
-        })
-    })
-})
+        expect(json).toEqual({"children": [{"contents": "<p>First post! Blogging is now enabled using Markdown!</p>", "extension": ".md", "name": "first-post.md", "path": "C:\\source\\rogue-blog\\src\\__tests__\\content\\first-post.md", "size": 101, "slug": "first-post", "title": "First Post!", "type": "file"}, {"children": [{"contents": "<h1>Bryan's Tech Blog</h1><p>Lorem</p>", "extension": ".md", "name": "home.md", "path": "C:\\source\\rogue-blog\\src\\__tests__\\content\\home\\home.md", "size": 28, "type": "file"}, {"children": [{"contents": "<h1>Bryan's Tech Blog</h1><p>Lorem</p>", "extension": ".md", "name": "second.md", "path": "C:\\source\\rogue-blog\\src\\__tests__\\content\\home\\second\\second.md", "size": 28, "type": "file"}], "name": "second", "path": "C:\\source\\rogue-blog\\src\\__tests__\\content\\home\\second", "size": 28, "type": "directory"}], "name": "home", "path": "C:\\source\\rogue-blog\\src\\__tests__\\content\\home", "size": 56, "type": "directory"}], "name": "content", "path": "C:\\source\\rogue-blog\\src\\__tests__\\content", "size": 157, "type": "directory"});
+    });
+});
